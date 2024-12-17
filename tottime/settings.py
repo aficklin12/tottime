2cd7abc74ac45b4f46b95d8265f7339ad332414e
aficklin12 <aficklin12@gmail.com>
2024-12-17 16:49:37 -0500

    fixed manifest link error

--- tottime/settings.py
+++ tottime/settings.py
@@ -2,6 +2,7 @@ import os
 import logging
 from pathlib import Path
 import stripe
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
