7a3df7cbdf75df2a056eacdbc6ef170127bc770c
Andrew Ficklin <aficklin12@gmail.com>
2025-06-10 16:52:17 -0400

    live 1.11.3

--- tottime/settings.py
+++ tottime/settings.py
@@ -130,7 +130,6 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 AUTH_USER_MODEL = 'tottimeapp.MainUser'
 
 
-
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'tot-time-media'
@@ -139,8 +138,6 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 # MEDIA_ROOT = BASE_DIR / 'media'
-
-
 SECURE_SSL_REDIRECT = False
 
 
